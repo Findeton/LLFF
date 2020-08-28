6e831358d4c73f5ddd53a62078199ef57128d732
Findeton <felrobelv@gmail.com>
2020-08-28 14:13:23 +0100

    wip

--- llff/poses/colmap_wrapper.py
+++ llff/poses/colmap_wrapper.py
@@ -64,7 +64,7 @@ def run_colmap(basedir, match_type):
         'colmap', 'mapper',
             '--database_path', os.path.join(basedir, 'database.db'),
             '--image_path', os.path.join(basedir, 'images'),
-            '--output_path', os.path.join(basedir, 'sparse'), # --export_path changed to --output_path in colmap 3.6
+            '--export_path', os.path.join(basedir, 'sparse'), # --export_path changed to --output_path in colmap 3.6
             '--Mapper.num_threads', '16',
             '--Mapper.init_min_tri_angle', '4',
             '--Mapper.multiple_models', '0',
