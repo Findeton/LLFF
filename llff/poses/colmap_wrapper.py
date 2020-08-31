da746aa969298bc6a001d62c23a6920d662acad1
Findeton <felrobelv@gmail.com>
2020-08-31 10:54:04 +0100

    docker

--- llff/poses/colmap_wrapper.py
+++ llff/poses/colmap_wrapper.py
@@ -26,7 +26,7 @@ def run_colmap(basedir, match_type):
     logfile = open(logfile_name, 'w')
     
     feature_extractor_args = [
-        'xvfb-run', '-s', '+extension GLX -screen 0 1024x768x24',
+        #'xvfb-run', '-s', '+extension GLX -screen 0 1024x768x24',
         'colmap', 'feature_extractor', 
             '--database_path', os.path.join(basedir, 'database.db'), 
             '--image_path', os.path.join(basedir, 'images'),
@@ -34,17 +34,19 @@ def run_colmap(basedir, match_type):
             '--ImageReader.single_camera', '1',
             '--SiftExtraction.estimate_affine_shape', '1',
             '--SiftExtraction.domain_size_pooling', '1',
-            '--SiftExtraction.use_gpu', '0',
+            #'--SiftExtraction.use_gpu', '0',
+            '--SiftExtraction.use_gpu', '1',
     ]
     feat_output = ( subprocess.check_output(feature_extractor_args, universal_newlines=True) )
     logfile.write(feat_output)
     print('Features extracted')
 
     exhaustive_matcher_args = [
-        'xvfb-run', '-s', '+extension GLX -screen 0 1024x768x24',
+        #'xvfb-run', '-s', '+extension GLX -screen 0 1024x768x24',
         'colmap', match_type, 
             '--database_path', os.path.join(basedir, 'database.db'),
-            '--SiftMatching.use_gpu', '0',
+            #'--SiftMatching.use_gpu', '0',
+            '--SiftMatching.use_gpu', '1',
     ]
 
     match_output = ( subprocess.check_output(exhaustive_matcher_args, universal_newlines=True) )
@@ -64,7 +66,7 @@ def run_colmap(basedir, match_type):
     #         '--Mapper.init_min_tri_angle', '4',
     # ]
     mapper_args = [
-        'xvfb-run', '-s', '+extension GLX -screen 0 1024x768x24',
+        #'xvfb-run', '-s', '+extension GLX -screen 0 1024x768x24',
         'colmap', 'mapper',
             '--database_path', os.path.join(basedir, 'database.db'),
             '--image_path', os.path.join(basedir, 'images'),
